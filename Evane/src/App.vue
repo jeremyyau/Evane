a950e7f71c03e1b2dc38006585b335fd977f9404
KwokYingFai <83331837+jeremyyau@users.noreply.github.com>
2024-06-01 22:51:49 +0800

    has bug

---
+++ Evane/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { Client } from "@gradio/client";
+</script>
+<script>
+const client = await Client.connect("KingNish/OpenGPT-4o");
+console.log(client);
+
+const result = await client.predict("/chat", { 		
+  message: {"text":"sample text","files":[]},
+  request: "idefics2-8b-chatty",
+  param_3: "Greedy",
+  param_4: 0,
+  param_5: 2048,
+  param_6: 0.01,
+  param_7: 0.01,
+  audio: null,
+  image3: null,
+});
+
+console.log(result.data);
+</script>
+
+<template>
+  <header>
+    
+  </header>
+
+  <main>
+  </main>
+</template>
