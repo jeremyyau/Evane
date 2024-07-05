5ae8d5a071c0696ac63396303cac31066b88d3a6
KwokYingFai <jeremyyaucf@gmail.com>
2024-07-05 16:48:17 +0800

    update API

--- Evane/src/App.vue
+++ Evane/src/App.vue
@@ -2,28 +2,19 @@
 import { Client } from "@gradio/client";
 </script>
 <script>
-// const client = await Client.connect("KingNish/OpenGPT-4o");
-// console.log(client);
+const client = await Client.connect("KingNish/OpenGPT-4o");
+console.log(client);
 
-// const result = await client.predict("/chat", { 		
-//   message: {"text":"sample text","files":[]},
-//   request: "idefics2-8b-chatty",
-//   param_3: "Greedy",
-//   param_4: 0,
-//   param_5: 2048,
-//   param_6: 0.01,
-//   param_7: 0.01,
-//   audio: null,
-//   image3: null,
-// });
-
-// console.log(result);
-const app = await Client.connect("KingNish/JARVIS");
-const result = await app.predict("/predict_1", [		
-				"用繁體中文回答我，你是建基於甚麼model？", // string  in 'User' Textbox component		
-				"Mixtral 8x7B", // string  in 'Model' Dropdown component		
-				0, // number (numeric value between 0 and 999999) in 'Seed' Slider component
-]);
+const result = await client.predict("/chat", { 		
+  user_prompt: {"text":"Hello!","files":[]}, 		
+  model_selector: "idefics2-8b-chatty", 		
+  decoding_strategy: "Greedy",
+  temperature: 0, 		
+  max_new_tokens: 2048, 		
+  repetition_penalty: 0.01, 		
+  top_p: 0.01, 		
+  web_search: true
+});
 
 console.log(result.data);
 document.querySelector("#response").innerText = result.data;
